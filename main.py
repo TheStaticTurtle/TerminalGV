32272a19129f4462f6d4e9b7fd9f8489fa89453f
TheStaticTurtle <tuglersamuel@gmail.com>
2021-12-22 20:09:26 +0100

    Fix a refresh bug

--- main.py
+++ main.py
@@ -2,33 +2,44 @@
 #logging.basicConfig(level=logging.DEBUG)
 
 import socketio, sys
+import ui, time
 
 sio = socketio.Client()
+ui = ui.UI()
+
+ui.print()
 
 class PepitaNamespace(socketio.ClientNamespace):
-    def on_connect(self):
-        pass
+	def on_connect(self):
+		pass
 
-    def on_disconnect(self):
-        pass
+	def on_disconnect(self):
+		pass
 
-    def on_gps(self, data):
-        print(data)
+	def on_gps(self, data):
+		ui.gps = data
+		ui.print()
 
-    def on_connected_devices(self, data):
-        print(data)
+	def on_connected_devices(self, data):
+		ui.network["connected_devices"] = data
+		ui.print()
 
-    def on_internet_link_quality(self, data):
-        print(data)
+	def on_internet_link_quality(self, data):
+		ui.network["internet_link_quality"] = data
+		ui.print()
 
-    def on_trainDetails(self, data):
-        print(data["stops"][-1]["realDate"])
+	def on_trainDetails(self, data):
+		ui.stops = data["stops"]
+		ui.terminusDate = data["stops"][-1]["realDate"]
+		ui.print()
 
 sio.register_namespace(PepitaNamespace('/router/api/pepita'))
 
 @sio.on('connect')
 def connect():
-    print("I'm connected!")
+	print("I'm connected!")
 
-sio.connect('https://wifi.sncf/')
-sio.wait()
+while True:
+	sio.connect('https://wifi.sncf/')
+	sio.wait()
+
